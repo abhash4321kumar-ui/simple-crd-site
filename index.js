10e8259d0b5c97caa81bba756600bf99de2f7193
abhash4321kumar-ui <abhash4321kumar@gmail.com>
2026-06-06 21:01:02 +0530

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
-
 let myexpress = require('./src/srcindex')
 let databasefnc = require('./src/config/databasefile')
 
+let PORT = 8080;
+
 databasefnc()
 
-myexpress.listen(8080, function(req, res){
+myexpress.listen(PORT, function(req, res){
    console.log('server is running!')
 })
